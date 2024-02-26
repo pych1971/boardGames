efa8f27e792c02534ffb6ca1c696bf841f511614
pych1971 <pych1971@gmail.com>
2024-02-26 20:40:24 +0500

    2024-02-26-20-40

--- boardGames.py
+++ boardGames.py
@@ -1,13 +1,12 @@
 import json
 import urllib.request
 
-numberOfGame = input()
-# with urllib.request.urlopen("https://api.tesera.ru/games/1704184") as descentTesera:
-with urllib.request.urlopen(f"https://api.tesera.ru/games/{numberOfGame}") as descentTesera:
-    descentJsonTesera = json.load(descentTesera)
-    print(descentJsonTesera["game"]["title"], descentJsonTesera["game"]["n10Rating"])
-    bggDescent = descentJsonTesera["game"]["bggId"]
+number_of_game = input()
+with urllib.request.urlopen(f"https://api.tesera.ru/games/{number_of_game}") as game_Tesera:
+    game_JSON_Tesera = json.load(game_Tesera)
+    print(game_JSON_Tesera["game"]["title"], game_JSON_Tesera["game"]["n10Rating"])
+    game_BGG = game_JSON_Tesera["game"]["bggId"]
 
-with urllib.request.urlopen(f"https://bgg-json.azurewebsites.net/thing/{bggDescent}") as descentBGG:
-    descentJsonBGG = json.load(descentBGG)
-    print(descentJsonBGG["name"], descentJsonBGG["averageRating"], descentJsonBGG["bggRating"], descentJsonBGG["rank"])
+with urllib.request.urlopen(f"https://bgg-json.azurewebsites.net/thing/{game_BGG}") as descentBGG:
+    game_JSON_BGG = json.load(descentBGG)
+    print(game_JSON_BGG["name"], game_JSON_BGG["averageRating"], game_JSON_BGG["bggRating"], game_JSON_BGG["rank"])
