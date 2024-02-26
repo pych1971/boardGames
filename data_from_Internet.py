749999fb9c96375d26f1e96ef202cc53c7c1cee4
pych1971 <pych1971@gmail.com>
2024-02-26 21:33:20 +0500

    2024-02-26-21-33

--- data_from_Internet.py
+++ data_from_Internet.py
@@ -1,2 +1,19 @@
-class Test:
-    pass
+import json
+import urllib.request
+
+
+class BoardGameFromInternet:
+    def __init__(self, number):
+        self.number = number
+
+    def game_from_Tesera(self):
+        with urllib.request.urlopen(f"https://api.tesera.ru/games/{self.number}") as game_Tesera:
+            self.game_JSON_tesera = json.load(game_Tesera)
+            self.game_BGG = self.game_JSON_tesera["game"]["bggId"]
+            print(self.game_JSON_tesera["game"]["title"], self.game_JSON_tesera["game"]["n10Rating"])
+
+    def game_from_Tesera(self):
+        with urllib.request.urlopen(f"https://bgg-json.azurewebsites.net/thing/{self.game_BGG}") as game_BGG:
+            self.game_JSON_BGG = json.load(game_BGG)
+            print(self.game_JSON_BGG["name"], self.game_JSON_BGG["averageRating"], self.game_JSON_BGG["bggRating"],
+                  self.game_JSON_BGG["rank"])
