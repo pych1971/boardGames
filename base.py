1d20297013d56dcaedccab1e163cbd4d41f016ac
pych1971 <pych1971@gmail.com>
2024-02-26 21:42:53 +0500

    2024-02-26-21-42

---
+++ base.py
@@ -0,0 +1,5 @@
+import data_from_Internet
+
+game = input('Введите название игры с Тесера:')
+game_from_Internet = data_from_Internet.BoardGameFromInternet(game)
+game_from_Internet.game_from_Tesera_and_BGG()
