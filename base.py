3a3c21e9f92c9267b49fcaec6ba17b0d49c60c94
pych1971 <pych1971@gmail.com>
2024-02-26 23:16:34 +0500

    2024-02-26-21-56

--- base.py
+++ base.py
@@ -1,5 +1,6 @@
 from data_from_Internet import BoardGameFromInternet
 
-game = input('Введите название игры с Тесера: ')
+# game = input('Введите название игры с Тесера: ')
+game = 'bloodborne-board-game'
 game_from_Internet = BoardGameFromInternet(game)
 print(game_from_Internet.game_from_Tesera_and_BGG())
--- data_from_Internet.py
+++ data_from_Internet.py
@@ -1,4 +1,5 @@
 import json
+import xml.etree.ElementTree
 import urllib.request
 
 
@@ -10,6 +11,12 @@ class BoardGameFromInternet:
             self.game_BGG = self.game_JSON_Tesera["game"]["bggId"]
         with urllib.request.urlopen(f"https://bgg-json.azurewebsites.net/thing/{self.game_BGG}") as game_BGG:
             self.game_JSON_BGG = json.load(game_BGG)
+        with urllib.request.urlopen(
+                f"https://boardgamegeek.com/xmlapi2/thing?id={self.game_BGG}&stats=1") as game_BGG_XML:
+            tree = xml.etree.ElementTree.parse(game_BGG_XML)
+            root = tree.getroot()
+            weight = root.findall('.//*[@ranks]')
+            print(weight)
 
     def game_from_Tesera_and_BGG(self):
         return self.game_JSON_Tesera["game"]["title"], self.game_JSON_Tesera["game"]["n10Rating"], self.game_JSON_BGG[
