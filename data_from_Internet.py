9b4df74948ea7c9b94a2d19165a062d3374cea4d
pych1971 <pych1971@gmail.com>
2024-02-26 21:43:08 +0500

    2024-02-26-21-43

--- data_from_Internet.py
+++ data_from_Internet.py
@@ -3,17 +3,15 @@ import urllib.request
 
 
 class BoardGameFromInternet:
-    def __init__(self, number):
-        self.number = number
-
-    def game_from_Tesera(self):
+    def __init__(self, game):
+        self.number = game
         with urllib.request.urlopen(f"https://api.tesera.ru/games/{self.number}") as game_Tesera:
-            self.game_JSON_tesera = json.load(game_Tesera)
-            self.game_BGG = self.game_JSON_tesera["game"]["bggId"]
-            print(self.game_JSON_tesera["game"]["title"], self.game_JSON_tesera["game"]["n10Rating"])
-
-    def game_from_Tesera(self):
+            self.game_JSON_Tesera = json.load(game_Tesera)
+            self.game_BGG = self.game_JSON_Tesera["game"]["bggId"]
         with urllib.request.urlopen(f"https://bgg-json.azurewebsites.net/thing/{self.game_BGG}") as game_BGG:
             self.game_JSON_BGG = json.load(game_BGG)
-            print(self.game_JSON_BGG["name"], self.game_JSON_BGG["averageRating"], self.game_JSON_BGG["bggRating"],
-                  self.game_JSON_BGG["rank"])
+
+    def game_from_Tesera_and_BGG(self):
+        print(self.game_JSON_Tesera["game"]["title"], self.game_JSON_Tesera["game"]["n10Rating"])
+        print(self.game_JSON_BGG["name"], self.game_JSON_BGG["averageRating"], self.game_JSON_BGG["bggRating"],
+              self.game_JSON_BGG["rank"])
