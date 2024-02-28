670ae432701ef55c0d84cc4f395a78b6cc55b995
pych1971 <pych1971@gmail.com>
2024-02-28 21:17:19 +0500

    2024-02-28-21-17

--- base.py
+++ base.py
@@ -2,5 +2,5 @@ from data_from_Internet import BoardGameFromInternet
 
 for game in ('great-western-trail-second-edition', 'great-western-trail-argentina', 'great-western-trail-new-zealand'):
     # game = input('Введите название игры с Тесера: ')
-    game_from_Internet = BoardGameFromInternet(game)
+    game_from_Internet =  BoardGameFromInternet(game)
     print(game_from_Internet.game_from_Tesera_and_BGG())
