d293d250f0af6e3a13b024267d22d0e45aa417ef
pych1971 <pych1971@gmail.com>
2024-02-28 13:14:56 +0500

    2024-02-28-13-14

--- data_from_Internet.py
+++ data_from_Internet.py
@@ -9,15 +9,14 @@ class BoardGameFromInternet:
         with urllib.request.urlopen(f"https://api.tesera.ru/games/{self.game_Tesera}") as game_Tesera:
             self.game_JSON_Tesera = json.load(game_Tesera)
             self.game_BGG = self.game_JSON_Tesera["game"]["bggId"]
-        with urllib.request.urlopen(f"https://bgg-json.azurewebsites.net/thing/{self.game_BGG}") as game_BGG:
-            self.game_JSON_BGG = json.load(game_BGG)
         with urllib.request.urlopen(
                 f"https://boardgamegeek.com/xmlapi2/thing?id={self.game_BGG}&stats=1") as game_BGG_XML:
-            tree = xml.etree.ElementTree.parse(game_BGG_XML)
-            root = tree.getroot()
-            weight = root.findall('.//*[@ranks]')
-            print(weight)
+            self.tree = xml.etree.ElementTree.parse(game_BGG_XML)
 
     def game_from_Tesera_and_BGG(self):
-        return self.game_JSON_Tesera["game"]["title"], self.game_JSON_Tesera["game"]["n10Rating"], self.game_JSON_BGG[
-            "name"], self.game_JSON_BGG["averageRating"], self.game_JSON_BGG["bggRating"], self.game_JSON_BGG["rank"]
+        return self.game_JSON_Tesera["game"]["title"], self.game_JSON_Tesera["game"]["n10Rating"], self.tree.find(
+            './/item/name').get('value'), float(
+            self.tree.find('.//*ratings/average').get('value')), float(
+            self.tree.find('.//*ratings/bayesaverage').get('value')), float(
+            self.tree.find(".//*ratings/ranks/*[@type='subtype']").get('value')), float(
+            self.tree.find('.//*ratings/averageweight').get('value'))
