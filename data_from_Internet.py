52255c6bfead58c9cd82c3e1dcf8ea1fb8603de2
pych1971 <pych1971@gmail.com>
2024-03-11 16:56:29 +0500

    2024-03-11-16-56

--- data_from_Internet.py
+++ data_from_Internet.py
@@ -11,17 +11,18 @@ class BoardGameFromInternet:
             self.game_BGG = self.game_JSON_Tesera["game"]["bggId"]
 
             self.game_Tesera_title = self.game_JSON_Tesera["game"]["title"]
+            self.game_Tesera_average_rating = self.game_JSON_Tesera["game"]["ratingUser"]
             self.game_Tesera_rating = self.game_JSON_Tesera["game"]["n10Rating"]
 
         with urllib.request.urlopen(
                 f"https://boardgamegeek.com/xmlapi2/thing?id={self.game_BGG}&stats=1") as game_BGG_XML:
             self.tree_XML_BGG = xml.etree.ElementTree.parse(game_BGG_XML)
 
-            self.game_BGG_name = self.tree_XML_BGG.find('.//item/name').get('value')
+            self.game_BGG_name = self.tree_XML_BGG.find('.//item/*[@type="primary"]').get('value')
             self.game_BGG_average_rating = float(self.tree_XML_BGG.find('.//*ratings/average').get('value'))
             self.game_BGG_geek_rating = float(self.tree_XML_BGG.find('.//*ratings/bayesaverage').get('value'))
             self.game_BGG_rank = int(self.tree_XML_BGG.find(".//*ratings/ranks/*[@type='subtype']").get('value'))
             self.game_BGG_weight = float(self.tree_XML_BGG.find('.//*ratings/averageweight').get('value'))
 
     def game_from_Tesera_and_BGG(self):
-        return self.game_Tesera_title, self.game_Tesera_rating, self.game_BGG_name, self.game_BGG_average_rating, self.game_BGG_geek_rating, self.game_BGG_rank, self.game_BGG_weight
+        return self.game_Tesera_title, self.game_Tesera_average_rating, self.game_Tesera_rating, self.game_BGG_name, self.game_BGG_average_rating, self.game_BGG_geek_rating, self.game_BGG_rank, self.game_BGG_weight
