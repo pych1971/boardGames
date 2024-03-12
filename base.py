f5dfb2d1935b22edea86b821a74e50d9ffa9548d
pych1971 <pych1971@gmail.com>
2024-03-12 12:15:38 +0500

    2024-03-12-12-15

--- base.py
+++ base.py
@@ -1,6 +1,6 @@
-from data_from_Internet import BoardGameFromInternet
+from data_from_Internet import BoardGameFromInternet, UserFromInternet
 
-user_tesera = 'pych1971'
+user_from_internet = UserFromInternet('pych1971')
 
 game_from_internet = BoardGameFromInternet(2199596)
 print(game_from_internet.game_from_tesera_and_bgg())
--- data_from_Internet.py
+++ data_from_Internet.py
@@ -4,8 +4,8 @@ import urllib.request
 
 
 class BoardGameFromInternet:
-    def __init__(self, game):
-        self.tesera_id = game
+    def __init__(self, tesera_id):
+        self.tesera_id = tesera_id
         with urllib.request.urlopen(f"https://api.tesera.ru/games/{self.tesera_id}") as game_tesera_json:
             self.game_tesera = json.load(game_tesera_json)
             self.bgg_id = self.game_tesera["game"]["bggId"]
@@ -25,4 +25,21 @@ class BoardGameFromInternet:
             self.bgg_weight = float(self.game_bgg.find('.//*ratings/averageweight').get('value'))
 
     def game_from_tesera_and_bgg(self):
-        return self.tesera_name, self.tesera_rating_user, self.tesera_n10_rating, self.bgg_name, self.bgg_average_rating, self.bgg_bayes_average_rating, self.bgg_rank, self.bgg_weight
+        return (
+            self.tesera_id, self.tesera_name, self.tesera_rating_user, self.tesera_n10_rating, self.bgg_id,
+            self.bgg_name,
+            self.bgg_average_rating, self.bgg_bayes_average_rating, self.bgg_rank, self.bgg_weight)
+
+
+class UserFromInternet:
+    def __init__(self, user_alias):
+        self.tesera_user_alias = user_alias
+        with urllib.request.urlopen(f"https://api.tesera.ru/user/{self.tesera_user_alias}") as user_tesera_json:
+            self.user_tesera = json.load(user_tesera_json)
+            self.games_in_collection = self.user_tesera['gamesInCollection']
+        for page in range(self.games_in_collection // 100 + 1):
+            with urllib.request.urlopen(
+                    f"https://api.tesera.ru/collections/base/own/{self.tesera_user_alias}?Limit=100&Offset={page}") as user_tesera_json:
+                self.next_page = json.load(user_tesera_json)
+                for game in self.next_page:
+                    print(BoardGameFromInternet(game['game']['teseraId']).game_from_tesera_and_bgg())
